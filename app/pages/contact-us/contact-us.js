bdef1d7b5a62c23723131af7d6cfd199804875d5
JelleDBZ <jelle.debuyzere@hotmail.be>
2019-09-26 11:59:38 +0200

    Added new pages for app

---
+++ app/pages/contact-us/contact-us.js
@@ -0,0 +1,5 @@
+const dialogModule = require('ui/dialogs');
+
+exports.onSubmit = function() {
+    dialogModule.alert('Submitted message succesfully');
+}
--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,6 +1,9 @@
 const frameModule = require('ui/frame');
 
-exports.onAboutTap = function(){
-    console.log('Going to about');
-    frameModule.topmost().navigate('pages/about/about');
+let navigate = function(page){
+    console.log(`Going to ${page}`);
+    frameModule.topmost().navigate(`pages/${page}/${page}`);
 }
+exports.onAboutTap = () => navigate('about');
+exports.onContactTap = () => navigate('contact-us');
+exports.onReposTap = () =>navigate('repos');
